be85abd08061a4a7fe80092f93ae7418f25e29fc
jbarinan <barinanjoshua@gmail.com>
2026-04-22 11:08:30 +0800

    Update landing pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: "export",
+  basePath: "/github-portfolio",
+  assetPrefix: "/github-portfolio/",
 };
 
 export default nextConfig;
